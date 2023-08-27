d4b5e5ea0cd213ac2f9383201608d0feece41da7
Vivek Srivastava <srivastavavivek012@gmail.com>
2023-08-27 13:15:24 +0530

    fixed skeleton

--- src/components/products/ProductCardSkeleton.tsx
+++ src/components/products/ProductCardSkeleton.tsx
@@ -5,7 +5,6 @@ const ProductCardSkeleton: React.FC = () => {
     <div className="product-card-skeleton">
       <div className="image" />
       <p className="title_1" />
-      <p className="title_2" />
       <p className="price" />
       <p className="stars" />
     </div>
